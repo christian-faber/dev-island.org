4e12cf4d33fa5bee44bb22a5d47a68c2162f1490
Christian <christian.a.faber@gmail.com>
2023-02-06 01:58:42 -1000

    okay final for real (just kidding)

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -3,7 +3,7 @@ import { ApplyForm } from "../components/ApplyForm";
 export const Apply = () => {
   return (
     <div className="flex flex-col justify-center bg-seaside">
-      <div className="text-center bg-coolgray border-white border m-10 py-8 shadow-lg">
+      <div className="text-center bg-coolgray border-white border m-10 py-8 shadow-lg rounded-xl">
         <h1 className="text-3xl font-bold drop-shadow-xl uppercase">Become</h1>
         <h1 className="text-3xl font-bold drop-shadow-xl uppercase">A</h1>
         <h1 className="text-3xl font-bold drop-shadow-xl uppercase">web</h1>
